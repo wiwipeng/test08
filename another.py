88f79995a6b7fe4d63c39972f1ba164df5aab776
wiwipeng <95857611+wiwipeng@users.noreply.github.com>
2021-12-16 21:32:50 +0800

    Update another.py

--- another.py
+++ another.py
@@ -7,7 +7,7 @@ a = b = n = 0     # 設定 a、b、n 三個變數，預設值 0
 while a!=4:       # 使用 while 迴圈，直到 a 等於 4 才停止
   a = b = n = 0   # 每次重複時將 a、b、n 三個變數再次設定為 0
   user = st.number_input('輸入四個數字：')  # 讓使用者輸入數字，並透過 list 轉換成串列
-   for i in user :
+  for i in user :
     if int(user[n]) == answer[n]:    # 因為使用者輸入的是「字串」，透過 int 轉換成數字，和答案串列互相比較
       a += 1                         # 如果位置和內容都相同，就將 a 增加 1
     else:
